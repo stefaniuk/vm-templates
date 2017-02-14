073abfbbff7e5478734a4cbedeb3263fe07a2ef3
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2017-02-14 23:50:54 +0000

    Disable SELinux

--- scientific-6.8/scripts/00-base.sh
+++ scientific-6.8/scripts/00-base.sh
@@ -3,5 +3,8 @@ set -ex
 
 [[ ! "$BUILD_SCRIPT_BASE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]] && exit
 
+# turn off SELinux
+echo "SELINUX=disabled" > /etc/sysconfig/selinux
+
 # turn off sshd DNS lookup to prevent timeout delay
 echo "UseDNS no" >> /etc/ssh/sshd_config
