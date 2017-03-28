a2192467f6b3724e4ff28fca7d3d5178c6720c2f
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2017-03-28 10:27:42 +0100

    Typo

--- scientific-7.2/scripts/00-base.sh
+++ scientific-7.2/scripts/00-base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-[[ ! "$BUILD_SCRIPT_BASE" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]] && exit
+[[ ! "$BUILD_SCRIPT_BASE" =~ ^(true|yes|on|1|TRUE|YES|ON)$ ]] && exit
 
 # turn off SELinux
 echo "SELINUX=disabled" > /etc/sysconfig/selinux
--- scientific-7.2/scripts/03-cleanup.sh
+++ scientific-7.2/scripts/03-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-[[ ! "$BUILD_SCRIPT_CLEANUP" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]] && exit
+[[ ! "$BUILD_SCRIPT_CLEANUP" =~ ^(true|yes|on|1|TRUE|YES|ON)$ ]] && exit
 
 yum --assumeyes clean all
 rm -rfv \
