783ba23ffa6ed4f9336e4924bddaf3bfd6b86b72
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2018-01-27 14:24:33 +0000

    Fix installation of VirtualBox Guest Additions

--- ubuntu-16.04/scripts/02-virtualbox.sh
+++ ubuntu-16.04/scripts/02-virtualbox.sh
@@ -3,8 +3,7 @@ set -ex
 
 [[ ! "$BUILD_SCRIPT_VIRTUALBOX" =~ ^(true|yes|on|1|TRUE|YES|ON)$ ]] && [ $PACKER_BUILDER_TYPE != "virtualbox" ] exit
 
-# install guest additions
-mount -o loop /home/${SSH_USERNAME}/VBoxGuestAdditions.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run ||:
-umount /mnt
-rm -fv /home/${SSH_USERNAME}/{VBoxGuestAdditions.iso,.vbox_version}
+# install guest packages
+apt-get --yes --ignore-missing --no-install-recommends install \
+    virtualbox-guest-dkms \
+    virtualbox-guest-utils
